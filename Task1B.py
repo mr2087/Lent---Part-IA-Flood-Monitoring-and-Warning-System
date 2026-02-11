dbd3092f2b85d5294c8504a3c27dfb5df8ad3371
noofne-cam <sl2335@cam.ac.uk>
2026-02-11 20:19:19 +0000

    Added tests to tasks 1B, 1F

--- Task1B.py
+++ Task1B.py
@@ -4,6 +4,9 @@ from floodsystem.stationdata import build_station_list
 # Import my function
 from floodsystem.geo import stations_by_distance
 
+# for unit testing
+from floodsystem.geo import _get_distance
+
 def dem_program():
     # Input Cambridge city centre coordinate, which will be p
     cambridge_centre = (52.2053, 0.1218)
@@ -26,9 +29,18 @@ def dem_program():
 # PYTEST Unit Tests
 def test_1B1():
     stations = build_station_list()
+    cambridge_cc = (52.2053, 0.1218)
     
     # test of function
-    stations_distance_sort = stations_by_distance(stations, (52.2053, 0.1218))
+    stations_distance_sort = stations_by_distance(stations, cambridge_cc)
+
+    # check that stations are actually sorted, m1
+    for i in range(len(stations_distance_sort)-1):
+        assert stations_distance_sort[i][1] <= stations_distance_sort[i+1][1]
+
+    # check that stations are sorted, m2
+    test_stations_distance = sorted(stations_distance_sort, key = lambda x : x[1])
+    assert test_stations_distance == stations_distance_sort
     
 if __name__ == "__main__":
     dem_program()
--- Task1F.py
+++ Task1F.py
@@ -1,6 +1,9 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
 
+# for unit test
+from floodsystem.station import MonitoringStation
+
 def stations_with_inconsistent_typical_range():
     
     # Get list of stations
@@ -13,6 +16,32 @@ def stations_with_inconsistent_typical_range():
     station_names = sorted(station.name for station in inconsistent)
     
     print(station_names)
+
+def test_1F1():
+    stations = build_station_list(use_cache = True)
+
+    # test written function
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+
+def test_1F2():
+    # manual testing
+    
+    stations = [
+        MonitoringStation('---', '---', 'Station A', (0, 0), None, '', ''),         # data totally not present, should trip
+        MonitoringStation('---', '---', 'Station B', (0, 0), (1.0, None), '', ''),     # data partially not present, should trip
+        MonitoringStation('---', '---', 'Station C', (0, 0), (1.2, 3.5), '', ''),    # data alright, should not trip
+        MonitoringStation('---', '---', 'Station D', (0, 0), (0, 0.0), '', ''),      # could happen in practice, should not trip
+        MonitoringStation('---', '---', 'Station E', (0, 0), (2.7, 2.6), '', ''),     # inconsistent data
+        MonitoringStation('---', '---', 'Station F', (0, 0), (1.4, -2.3), '', '')       # should work with negative numbers, should trip
+    ]
+
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+
+    for station in stations:
+        if station.name in ['Station A', 'Station B', 'Station E', 'Station F']:
+            assert station in inconsistent_stations
+        else:
+            assert station not in inconsistent_stations
 
 if __name__ == "__main__":
     stations_with_inconsistent_typical_range()
