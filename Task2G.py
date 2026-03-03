0c99f4b30a80a88e7a7787c8e1ce99dcd002832e
noofne-cam <sl2335@cam.ac.uk>
2026-03-03 15:16:23 +0000

    Adjusted flooding model in 2G

--- Task2G.py
+++ Task2G.py
@@ -25,7 +25,7 @@ def forecast_water_level_sign(station, short, long) -> None:
     print(f"Fetching measure levels for station {station.name} at {station.town} measuring {station.river} ...")
     dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(long))
     if dates == [] or levels == []:
-        return (None, None)
+        return (None, None, None)
 
     print(f"Obtaining linear fit for data for station {station.name} ...")
     poly_l, _ = get_water_level_fit(dates, levels, 1)
@@ -35,11 +35,11 @@ def forecast_water_level_sign(station, short, long) -> None:
     poly_s, _ = get_water_level_fit(dates[:timescale_factor], levels[:timescale_factor], 1)
     coeff_s = poly_s.coefficients[0]
 
-    return (np.sign(coeff_l if abs(coeff_l) > 0.05 else 0), 
+    return (np.sign(coeff_l if abs(coeff_l) > 0.05 else 0), coeff_l, 
             np.sign(coeff_s if abs(coeff_s) > 0.05 else 0))
 
 def evaluate_flooding(station) -> tuple[float, str]:
-    frwl_short, frwl_long = forecast_water_level_sign(station, 0.5, 10)
+    frwl_short, frwl_long_mag, frwl_long = forecast_water_level_sign(station, 0.5, 30)
     rwl = station.relative_water_level()
 
     if any([q == None for q in (frwl_short, frwl_long, rwl)]):
@@ -47,17 +47,17 @@ def evaluate_flooding(station) -> tuple[float, str]:
         return (None, None)
     
     # calibration factors
-    rwl_factor = 0.1
-    rwl_augment = 0.35
+    rwl_factor = 0.05
+    rwl_augment = 0.38
 
-    short_grad_factor = 0.1 + (0.4 if rwl > 0.7 else 0)
-    long_grad_factor = 0.005
+    short_grad_factor = 0.05 + (0.3 if rwl > 0.85 else 0)
+    print(frwl_long_mag)
+    long_grad_factor = abs(frwl_long_mag)*0.1
 
     # score evaluation
-    score = ( (rwl) * (rwl_factor + (rwl_augment if rwl > 0.7 else 0)) + 
-                ((frwl_short + (0.90 if rwl > 1 else 0))) * short_grad_factor + 
-                frwl_long * long_grad_factor) / \
-    (rwl_factor + short_grad_factor + long_grad_factor + (rwl_augment if rwl > 0.7 else 0))
+    score = ( ((rwl-0.6) * (rwl_factor + (rwl_augment if rwl > 0.8 else 0))) + 
+                ((frwl_short + (0.70 if rwl > 1.2 else 0)) * short_grad_factor) + 
+                (frwl_long-0.5) * long_grad_factor)
 
     print(f'[{station.name.upper()}] short term water level: {'rising' if frwl_short > 0 else 'falling' if frwl_short < 0 else 'steady'}')
     print(f'[{station.name.upper()}]  long term water level: {'rising' if frwl_long > 0 else 'falling' if frwl_long < 0 else 'steady'}')
@@ -67,8 +67,8 @@ def evaluate_flooding(station) -> tuple[float, str]:
     print(f'[{station.name.upper()}] measured water level: {station.latest_level}')
 
     status = 'LOW/NONE' if score < 0.05 else \
-            'MODERATE' if 0.05 <= score < 0.10 else \
-            'HIGH' if 0.10 <= score < 0.25 else \
+            'MODERATE' if 0.05 <= score < 0.20 else \
+            'HIGH' if 0.20 <= score < 0.50 else \
             'SEVERE'
 
     return score, status
@@ -80,7 +80,7 @@ def main() -> None:
     warning_stations = [station_data[0] for station_data in stations_level_over_threshold(stations, 0.3)]
     print(f'Testing {len(warning_stations)} higher-risk stations ...\n\n')
 
-    for station in stations[250:265]:
+    for station in stations[340:355]:
         score, status = evaluate_flooding(station)
         
         # report
